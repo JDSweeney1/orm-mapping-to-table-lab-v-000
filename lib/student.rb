48a3cfe4068dcecdca1e9c9b50495605e92bd7a5
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-16 23:37:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,4 +5,14 @@ class Student
   def initialize(name, grade, id = nil)
     @id, @name, @grade = id, name, grade
   end
+
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:CONN].execute(sql)
 end
